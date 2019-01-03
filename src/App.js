7c2f172e7f2ad42c98a0de456d70cd70f435de02
“Kit <kit@crystaldelta.com>
2019-01-03 12:44:00 +1100

    add delete handler

--- src/App.js
+++ src/App.js
@@ -12,18 +12,6 @@ class App extends Component {
     otherState:'some other value'
   }
 
-  switchNameHandler = (newName) => {
-
-    this.setState({
-      persons : [
-        {name: newName, age:34},
-        {name: "Stanley", age:33},
-        {name: "Edina", age:29}
-      ],
-      showPersons: false
-    })
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons : [
@@ -32,6 +20,13 @@ class App extends Component {
         {name: "Edina", age:29}
       ]
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+
   }
 
   togglePersonsHandler = () => {
@@ -52,8 +47,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
+            click={() => this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}/>
           })}
